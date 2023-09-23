21a51ba64a8e852f2c707596ab67734722362866
N-atra <n.a.patrakhina@gmail.com>
2023-09-23 23:58:44 +0600

    Добавлен модуль в проект IDEA

---
+++ sandbox/src/main/java/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * @author <a href="mailto:d.zhluktov@gmail.com">Dmitrii Zhluktov</a>
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
